bb4fd4421f34a00ac194713485b3a845f880dd25
Doug Blanding <dblanding@gmail.com>
2024-04-05 20:28:13 -0400

    add servo_test.py

---
+++ Pico_code/servo_test.py
@@ -0,0 +1,23 @@
+from machine import Pin,PWM
+import utime
+
+STEP = 100_000
+MIN = 900_000
+MID = 1_500_000
+MAX = 2_100_000
+
+pwm = PWM(Pin(15))
+
+pwm.freq(50)
+pwm.duty_ns(MID)
+print(MID)
+utime.sleep(1)
+
+value = MIN
+while value <= MAX:
+    print(value)
+    pwm.duty_ns(value)
+    value += STEP
+    utime.sleep(1)
+pwm.duty_ns(MID)
+print(MID)
